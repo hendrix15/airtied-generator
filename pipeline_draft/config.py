f1d27cde2ecbe0e61f6a00c444444a08d5e8c419
hendrix15 <hendrik.woelert@online.de>
2024-06-11 14:31:13 +0200

    adapt config: dont use material coefficients

--- pipeline_draft/config.py
+++ pipeline_draft/config.py
@@ -10,16 +10,16 @@ class Material:
     """Material used for Finite Element Analysis"""
 
     name = "Steel"
-    e = 29000  # (ksi) Modulus of elasticity
-    g = 11400  # (ksi) Shear modulus
-    nu = 0.30  # Poisson's ratio
-    rho = 0.000283  # (kci) Density
+    e = 1  # (ksi) Modulus of elasticity
+    g = 1  # (ksi) Shear modulus
+    nu = 1  # Poisson's ratio
+    rho = 1  # (kci) Density
 
 
 class SectionProperties:
     """Section Properties used for Finite Element Analysis"""
 
-    iy = 17.3  # (in**4) Weak axis moment of inertia
-    iz = 204  # (in**4) Strong axis moment of inertia
-    j = 0.300  # (in**4) Torsional constant
-    a = 7.65  # (in**2) Cross-sectional area
+    iy = 1  # (in**4) Weak axis moment of inertia # euler buckling -> look up table
+    iz = 1  # (in**4) Strong axis moment of inertia # 
+    j = 1  # (in**4) Torsional constant
+    a = 1 # (in**2) Cross-sectional area
