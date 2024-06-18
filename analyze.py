48d9a3b0b5b6f52255a87a08ac9969f6118c5dd4
hendrix15 <hendrik.woelert@online.de>
2024-06-18 16:22:35 +0200

    add force types

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from utils.fea import generate_FEA_truss, get_euler_load
+from utils.fea import ForceType, generate_FEA_truss, get_euler_load
 from utils.parser import read_json
 from utils.plot import visualize
 
@@ -14,15 +14,17 @@ def main() -> None:
     truss = generate_FEA_truss(nodes, edges)
     truss.analyze(check_statics=True)
     max_forces = {member.name: member.max_axial() for member in truss.Members.values()}
+    print(max_forces)
 
     exceeded_edges = []
     for edge in edges:
         max_force = max_forces[edge.id]
-        euler_load = get_euler_load(edge.length())
-        if max_force > euler_load:
+        force_type = ForceType.COMPRESSION if max_force > 0 else ForceType.TENSION
+        euler_load = get_euler_load(edge.length(), force_type)
+        if abs(max_force) > euler_load:
             exceeded_edges.append(edge.id)
             print(
-                f"Member {edge.id}: Max force of {max_force} exceeds the euler load of {euler_load}"
+                f"Member {edge.id}: Max force of {max_force}, {force_type} exceeds the euler load of {euler_load}"
             )
 
     print(
