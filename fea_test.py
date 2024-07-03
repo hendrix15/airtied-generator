e20e741cd33b489de6fb3b46087b9eaa99bb0b93
Stefan Spangenberg <stefan.spangenberg@student.hpi.de>
2024-07-03 16:55:46 +0200

    Some fea improvements and tests

---
+++ fea_test.py
@@ -0,0 +1,32 @@
+from utils.fea import fea_opensees, fea_pynite
+from utils.parser import read_json
+
+
+def main() -> None:
+    for file_name in [
+        "dino.json",
+        "triangle.json",
+        "simple_pyramid.json",
+        "complex_pyramid.json",
+    ]:
+        nodes, edges = read_json(f"output/{file_name}")
+        pynite_max_forces = fea_pynite(nodes, edges)
+        opensees_max_forces = fea_opensees(nodes, edges, False)
+        print("PyNite \n")
+        for edge, force in pynite_max_forces.items():
+            print(f"{edge} - {force}")
+        print("\n \n")
+        print("Opensees \n")
+        for edge, force in opensees_max_forces.items():
+            print(f"{edge} - {force}")
+        print("\n")
+        max_abs = max(
+            abs(opensees_max_forces[edge] - force)
+            for edge, force in pynite_max_forces.items()
+        )
+        print(f"Max diff: {max_abs}")
+        print("\n \n")
+
+
+if __name__ == "__main__":
+    main()
--- utils/fea.py
+++ utils/fea.py
@@ -13,21 +13,19 @@ from search.models import Edge, Node
 class Material:
     """Material used for Finite Element Analysis"""
 
-    name = "Custom"
-    e = 1  # Modulus of elasticity
-    g = 1  # Shear modulus
-    nu = 1  # Poisson's ratio
-    rho = (
-        1 / (math.pi * math.pow((0.2 / 2), 2) * 1) * 0.11 * 9.81
-    )  # (N per m^3) Density
+    name = "Steel"
+    e = 199.95  # (GPa) Modulus of elasticity
+    g = 78.60  # (GPa) Shear modulus
+    nu = 0.30  # Poisson's ratio
+    rho = 1 / (math.pi * math.pow((0.2 / 2), 2) * 1) * 0.11  # (N per m^3) Density
 
 
 class SectionProperties:
     """Section Properties used for Finite Element Analysis"""
 
-    iy = 1  # Weak axis moment of inertia
-    iz = 1  # Strong axis moment of inertia
-    j = 1  # Torsional constant
+    iy = 0.0000072  # (m**4) Weak axis moment of inertia
+    iz = 0.000085  # (m**4) Strong axis moment of inertia
+    j = 1.249e-7  # (m**4) Torsional constant
     a = math.pi * math.pow((0.2 / 2), 2)  # (m^2) Cross-sectional area
 
 
@@ -97,48 +95,105 @@ def fea_pynite(nodes: list[Node], edges: list[Edge]) -> dict:
     return max_forces
 
 
-def fea_opensees(nodes: list[Node], edges: list[Edge]) -> dict:
+def fea_opensees(nodes: list[Node], edges: list[Edge], truss: bool = False) -> dict:
     node_mapping = {node.id: i for i, node in enumerate(nodes)}
     edge_mapping = {edge.id: i for i, edge in enumerate(edges)}
 
     ops.wipe()
 
-    ops.model("basic", "-ndm", 3, "-ndf", 6)
+    if truss:
+        ops.model("basic", "-ndm", 3, "-ndf", 3)
+    else:
+        ops.model("basic", "-ndm", 3, "-ndf", 6)
+
+    ops.timeSeries("Constant", 1)
+    ops.pattern("Plain", 1, 1)
 
     for node in nodes:
         ops.node(node_mapping[node.id], node.vec.x, node.vec.y, node.vec.z)
 
-    ops.uniaxialMaterial("Elastic", 1, Material.e)
+    if truss:
+        # Reinforced steel
+        ops.uniaxialMaterial("Steel01", 1, 60.0, 30000.0, 0.01)
+    else:
+        # Transformation of local coordinate system
+        ops.geomTransf("Linear", 1, 0, 0, 1)
+        ops.geomTransf("Linear", 2, 1, 0, 0)
+        # Calculate self weight of the beams
+        b = [0, -Material.rho * SectionProperties.a, 0]
+        wx = np.dot(ops.eleResponse(1, "xaxis"), b)  # x'*b
+        wy = np.dot(ops.eleResponse(1, "yaxis"), b)  # y'*b
+        wz = np.dot(ops.eleResponse(1, "zaxis"), b)  # z'*b
 
     for edge in edges:
-        ops.element(
-            "Truss",
-            edge_mapping[edge.id],
-            node_mapping[edge.u.id],
-            node_mapping[edge.v.id],
-            SectionProperties.a,
-            1,
-        )
+        if truss:
+            ops.element(
+                "Truss",
+                edge_mapping[edge.id],
+                node_mapping[edge.u.id],
+                node_mapping[edge.v.id],
+                SectionProperties.a,
+                1,
+            )
+        else:
+            # Select transformation
+            transform = (
+                2
+                if edge.u.vec.x == edge.v.vec.x and edge.u.vec.y == edge.v.vec.y
+                else 1
+            )
+            ops.element(
+                "elasticBeamColumn",
+                edge_mapping[edge.id],
+                node_mapping[edge.u.id],
+                node_mapping[edge.v.id],
+                SectionProperties.a,
+                Material.e,
+                Material.g,
+                SectionProperties.j,
+                SectionProperties.iz,
+                SectionProperties.iy,
+                transform,
+            )
+            # Add self weight
+            ops.eleLoad(
+                "-ele", edge_mapping[edge.id], "-type", "-beamUniform", wy, wz, wx
+            )
 
     for node in nodes:
         if node.r_support and node.t_support:
-            ops.fix(
-                node_mapping[node.id],
-                int(node.t_support.x),
-                int(node.t_support.y),
-                int(node.t_support.z),
-                int(node.r_support.x),
-                int(node.r_support.y),
-                int(node.r_support.z),
-            )
+            if truss:
+                ops.fix(
+                    node_mapping[node.id],
+                    int(node.t_support.x),
+                    int(node.t_support.y),
+                    int(node.t_support.z),
+                )
+            else:
+                ops.fix(
+                    node_mapping[node.id],
+                    int(node.t_support.x),
+                    int(node.t_support.y),
+                    int(node.t_support.z),
+                    int(node.r_support.x),
+                    int(node.r_support.y),
+                    int(node.r_support.z),
+                )
 
-    ops.timeSeries("Constant", 1)
-    ops.pattern("Plain", 1, 1)
     for node in nodes:
         if node.load:
-            ops.load(
-                node_mapping[node.id], node.load.x, node.load.y, node.load.z, 0, 0, 0
-            )
+            if truss:
+                ops.load(node_mapping[node.id], node.load.x, node.load.y, node.load.z)
+            else:
+                ops.load(
+                    node_mapping[node.id],
+                    node.load.x,
+                    node.load.y,
+                    node.load.z,
+                    0,
+                    0,
+                    0,
+                )
 
     ops.system("BandSPD")
     ops.numberer("RCM")
@@ -146,9 +201,15 @@ def fea_opensees(nodes: list[Node], edges: list[Edge]) -> dict:
     ops.integrator("LoadControl", 1.0)
     ops.algorithm("Linear")
     ops.analysis("Static")
-    ops.analyze(1)
 
-    max_forces = {edge.id: ops.basicForce(edge_mapping[edge.id])[0] for edge in edges}
+    result = ops.analyze(1)
+    if result == -3:  # Ax=b failed
+        for nd in ops.getNodeTags():
+            print(f"Node {nd}: {ops.nodeDOFs(nd)}")
+
+    max_forces = {
+        edge.id: ops.basicForce(edge_mapping[edge.id])[0] * -1 for edge in edges
+    }
     return max_forces
 
 
